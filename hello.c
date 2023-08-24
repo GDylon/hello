60a60688e58e1d7c7efdb778fbf9e5fb31433774
Dylon George <dgeorge4@huskers.unl.edu>
2023-08-24 13:35:35 -0500

    added my major to output

---
+++ hello.c
@@ -0,0 +1,18 @@
+/**
+ * Author: Dylon George
+ *         EMAIL
+ * Date: 2023/8/24
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Dylon George\n");
+  printf("Electrical Engineering.\n");
+
+  return 0;
+}
